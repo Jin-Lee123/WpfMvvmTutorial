1793e1c2d6906678f7ecba3359d942c874f0d8a6
Jin-Lee123 <wls4310@koreatech.ac.kr>
2021-09-02 15:14:01 +0900

    초기 커밋.

---
+++ WpfMvvmApp/ViewModels/RelayCommand.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace WpfMvvmApp
+{
+    public class RelayCommand<T> : ICommand
+    {
+        readonly Action<T> execute;          // View에서 넘어온 Command를 실행하는 대리자
+        readonly Predicate<T> canExecute;    // View에서 넘어온 Command를 실행할수 있는지 확인 대리자
+
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            if (canExecute.Invoke((T)parameter))
+                return true;
+            else
+                return false;
+        }
+
+        public void Execute(object parameter)
+        {
+            execute.Invoke((T)parameter);
+        }
+
+        public RelayCommand(Action<T> execute, Predicate<T> canExecute)
+        {
+            // RelayCommand 실행명령은 null일 수 없음
+            this.execute = execute ?? throw new ArgumentNullException(nameof(execute));
+            this.canExecute = canExecute;
+        }
+
+        public RelayCommand(Action<T> execute) : this(execute, null) { }
+    }
+}
---
+++ WpfMvvmApp/ViewModels/ViewModelBase.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfMvvmApp.ViewModels
+{
+    public class ViewModelBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void RaisePropertyChanged(string propertyName)
+        {
+            // 속성값 변경시 이벤트 발생
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
